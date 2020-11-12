aad73572c18e2f30548a371ef34034079408b0ab
luisptmdf <luis@chicagobooth.edu>
2020-11-12 21:11:05 +0000

    as far as I went before breakout rooms

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -10,8 +10,8 @@
 #
 class Bookmark < ApplicationRecord
 
-  belongs_to(:movie, { :required => false, :class_name => "Movie", :foreign_key => "movie_id" })
+  belongs_to(:movie, { :required => true, :class_name => "Movie", :foreign_key => "movie_id" })
 
-  belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "user_id" })
-  
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id" })
+
 end
